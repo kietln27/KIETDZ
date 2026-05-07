dc58946a17ae305225b877f73c1cc73b7d5a045f
kietln27 <lenhankiet27@gmail.com>
2026-05-07 23:56:07 +0700

    Create CheckID.lua

---
+++ CheckID.lua
@@ -0,0 +1,5 @@
+-- Kiểm tra đúng ID Game trước khi chạy--
+if game.PlaceId == 116139828947259 then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/kietln27/KIETDZ/refs/heads/main/FarmGem.lua"))()
+else
+end
