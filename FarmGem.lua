3fe7ac5407c9f6a4bbadb174d488bbe357f52842
kietln27 <lenhankiet27@gmail.com>
2026-05-08 01:00:02 +0700

    Update FarmGem.lua

--- FarmGem.lua
+++ FarmGem.lua
@@ -10,19 +10,13 @@ local screenGui = Instance.new("ScreenGui", player:WaitForChild("PlayerGui"))
 screenGui.Name = "AutoFarmStatus"
 
 local statusLabel = Instance.new("TextLabel", screenGui)
-statusLabel.Size = UDim2.new(0, 320, 0, 60)
-statusLabel.Position = UDim2.new(0.5, -160, 0.1, 0) -- Đã chỉnh lại -160 để căn giữa chuẩn
+statusLabel.Size = UDim2.new(0, 300, 0, 50)
+statusLabel.Position = UDim2.new(0.5, -150, 0.1, 0) -- Nằm ở giữa phía trên
 statusLabel.BackgroundColor3 = Color3.new(0, 0, 0)
 statusLabel.BackgroundTransparency = 0.5
 statusLabel.TextColor3 = Color3.new(1, 1, 1)
-statusLabel.TextScaled = false
-statusLabel.TextSize = 22 -- Tăng size chữ lên một chút cho dễ nhìn
-statusLabel.Font = Enum.Font.GothamBold -- Đổi font ngầu và hỗ trợ tiếng Việt
+statusLabel.TextScaled = true
 statusLabel.Text = "Đang khởi động..."
-
--- THÊM BO GÓC
-local uiCorner = Instance.new("UICorner", statusLabel)
-uiCorner.CornerRadius = UDim.new(0, 12) -- Tăng độ bo góc lên 15 cho mượt hơn
 
 -- Hàm để đổi chữ nhanh
 local function updateStatus(text)
