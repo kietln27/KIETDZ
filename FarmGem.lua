73e2f120a312439f1d6d76f91b902e677ae083e6
kietln27 <lenhankiet27@gmail.com>
2026-05-08 12:04:05 +0700

    Update FarmGem.lua

--- FarmGem.lua
+++ FarmGem.lua
@@ -238,7 +238,7 @@ local function startAutoFarm()
             if dist < 5 then
                 hum.WalkSpeed = 0
             elseif isInsideWall then
-                hum.WalkSpeed = 100
+                hum.WalkSpeed = 30
             else
                 hum.WalkSpeed = 30
             end
